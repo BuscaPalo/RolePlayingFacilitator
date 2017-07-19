d47069a71274471be09e918c00d524b54b0f363a
Garrent <diegopousalp@gmail.com>
2017-07-19 00:33:56 -0300

    Nuevos UserControls para el layout

---
+++ Visual/Personaje.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RPF.Visual
+{
+    /// <summary>
+    /// Lógica de interacción para Personaje.xaml
+    /// </summary>
+    public partial class Personaje : UserControl
+    {
+        public Personaje()
+        {
+            InitializeComponent();
+            //TODO: Setear bien con la DB
+            in_nombre.DataContext = new InputData
+            {
+                TypeData = "Comete esta",
+                Data = "forro"
+            };
+            Num = 100;
+        }
+
+        public string Hola { get; set; }
+        public int Num { get; set; }
+    }
+}
---
+++ Visual/TextInfoInput.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RPF.Visual
+{
+    /// <summary>
+    /// Lógica de interacción para TextInfoInput.xaml
+    /// </summary>
+    public partial class TextInfoInput : UserControl
+    {
+        public TextInfoInput()
+        {
+            InitializeComponent();
+            TypeData = "asd";
+            Data = "Chau";
+        }
+
+
+
+        public string TypeData
+        {
+            get { return (string)GetValue(TypeDataProperty); }
+            set
+            {
+                SetValue(TypeDataProperty, value);
+            }
+        }
+
+        // Using a DependencyProperty as the backing store for TypeData.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty TypeDataProperty =
+            DependencyProperty.Register("TypeData", typeof(string), typeof(TextInfoInput), new PropertyMetadata(""));
+
+        public string Data
+        {
+            get { return (string)GetValue(DataProperty); }
+            set
+            {
+                SetValue(DataProperty, value);
+            }
+        }
+
+        // Using a DependencyProperty as the backing store for Data.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty DataProperty =
+            DependencyProperty.Register("Data", typeof(string), typeof(TextInfoInput), new PropertyMetadata(""));
+
+
+
+    }
+}
